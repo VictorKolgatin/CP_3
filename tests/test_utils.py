dac6cbd8495815246bb42e9b307dca91baaa307f
Victor <kolgatin@gmail.com>
2023-05-30 16:31:55 +0300

    созданы тесты, в папке находится тестовый файл .json

---
+++ tests/test_utils.py
@@ -0,0 +1,95 @@
+from src.utils import load_file, executed_last_operations, format_operation
+
+file = 'test_operations.json'
+count = 1
+
+
+def test_load_file():
+    assert load_file('test_operations1.json') is None
+    assert len(load_file(file)) > 0
+    assert isinstance(load_file(file), list)
+    assert load_file('test_operations.json') == [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        }]
+
+
+def test_executed_last_operations():
+    data = load_file(file)
+    assert executed_last_operations(data, count) == [{'date': '2019-10-30T01:49:52.939296',
+                                                      'description': 'Перевод с карты на счет',
+                                                      'from': 'Visa Gold 7756673469642839',
+                                                      'id': 509645757,
+                                                      'operationAmount': {'amount': '23036.03',
+                                                                          'currency': {'code': 'RUB', 'name': 'руб.'}},
+                                                      'state': 'EXECUTED',
+                                                      'to': 'Счет 48943806953649539453'}]
+
+    assert executed_last_operations(data) == [{'date': '2019-10-30T01:49:52.939296',
+                                               'description': 'Перевод с карты на счет',
+                                               'from': 'Visa Gold 7756673469642839',
+                                               'id': 509645757,
+                                               'operationAmount': {'amount': '23036.03',
+                                                                   'currency': {'code': 'RUB', 'name': 'руб.'}},
+                                               'state': 'EXECUTED',
+                                               'to': 'Счет 48943806953649539453'},
+                                              {'date': '2019-08-26T10:50:58.294041',
+                                               'description': 'Перевод организации',
+                                               'from': 'Maestro 1596837868705199',
+                                               'id': 441945886,
+                                               'operationAmount': {'amount': '31957.58',
+                                                                   'currency': {'code': 'RUB', 'name': 'руб.'}},
+                                               'state': 'EXECUTED',
+                                               'to': 'Счет 64686473678894779589'},
+                                              {'date': '2019-06-14T19:37:49.044089',
+                                               'description': 'Перевод со счета на счет',
+                                               'from': 'Счет 73222753239048295679',
+                                               'id': 811920303,
+                                               'operationAmount': {'amount': '63150.74',
+                                                                   'currency': {'code': 'USD', 'name': 'USD'}},
+                                               'state': 'EXECUTED',
+                                               'to': 'Счет 78544755774551298747'},
+                                              {'date': '2018-12-29T21:45:18.495053',
+                                               'description': 'Перевод организации',
+                                               'from': 'Счет 77977573135347241529',
+                                               'id': 547682597,
+                                               'operationAmount': {'amount': '66263.93',
+                                                                   'currency': {'code': 'RUB', 'name': 'руб.'}},
+                                               'state': 'EXECUTED',
+                                               'to': 'Счет 33062909508148771891'}]
+
+
+def test_format_operation():
+    data = load_file(file)
+    filter_data = executed_last_operations(data)
+
+    assert format_operation(filter_data)
+
+    assert format_operation(filter_data) == ['\n'
+                                             '        30.10.2019 - Перевод с карты на счет\n'
+                                             '        Visa Gold 7756 67** **** 2839 -> Счет **9453\n'
+                                             '        23036.03 руб.',
+                                             '\n'
+                                             '        26.08.2019 - Перевод организации\n'
+                                             '        Maestro 1596 83** **** 5199 -> Счет **9589\n'
+                                             '        31957.58 руб.',
+                                             '\n'
+                                             '        14.06.2019 - Перевод со счета на счет\n'
+                                             '        Счет 7322 27** **** 5679 -> Счет **8747\n'
+                                             '        63150.74 USD',
+                                             '\n'
+                                             '        29.12.2018 - Перевод организации\n'
+                                             '        Счет 7797 75** **** 1529 -> Счет **1891\n'
+                                             '        66263.93 руб.']
