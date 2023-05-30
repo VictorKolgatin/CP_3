6595e9f5c6b3781a68e422296a27e8e7e7b1a585
Victor <kolgatin@gmail.com>
2023-05-30 15:49:22 +0300

    Файл для запуска программы

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from src.utils import load_file, executed_last_operations, format_operation
+
+FILENAME = "operations.json"
+count_last_operation = 5
+
+
+def main():
+    operations_file = load_file(FILENAME)
+    last_operation = executed_last_operations(operations_file, count_last_operation)
+    format_operation(last_operation)
+
+
+main()
